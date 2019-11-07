1d1999e0a1ad355d8a120321edc161b4aec17795
ICHx <ICHx@users.noreply.github.com>
2019-11-07 22:08:36 +0800

    7-11
    trying negative

---
+++ cmake-build-debug/CMakeFiles/data_struct19.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_struct19.dir/lab2/prj2-nov.c.obj"
+  "data_struct19.pdb"
+  "data_struct19.exe"
+  "data_struct19.exe.manifest"
+  "libdata_struct19.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/data_struct19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab2/prj2-nov.c
+++ lab2/prj2-nov.c
@@ -13,8 +13,8 @@ typedef float STACK_DATA; //using float to store both integer and float
 #define MAX 100
 
 struct node {
-	STACK_DATA data; // store single character
-	node_ptr next; //actually is previous node
+    STACK_DATA data; // store single character
+    node_ptr next; //actually is previous node
 };
 
 stack newStack();
@@ -36,257 +36,285 @@ short InfixConv(char *, char *);
 short evaluate(const char *, float *);
 
 stack newStack() {
-	stack stack0;
-	stack0 = malloc(sizeof(struct node));
-	if (stack0 == NULL) {
-		puts("OOM!");
-		exit(-1);
-	}
-	stack0->data = '\0';
-	stack0->next = NULL;
+    stack stack0;
+    stack0 = malloc(sizeof(struct node));
+    if (stack0 == NULL) {
+        puts("OOM!");
+        exit(-1);
+    }
+    stack0->data = '\0';
+    stack0->next = NULL;
 
-	return stack0;
+    return stack0;
 }
 
 void push(stack stack0, STACK_DATA c0) {
-	stack stack1 = newStack();
-	if (stack1 != NULL) {
-		stack1->data = c0;
-		stack1->next = stack0->next;
-		stack0->next = stack1;
-	}
+    stack stack1 = newStack();
+    if (stack1 != NULL) {
+        stack1->data = c0;
+        stack1->next = stack0->next;
+        stack0->next = stack1;
+    }
 }
 
 STACK_DATA pop(stack stack0) {
 
-	node_ptr tosp;
-	if (isEmpty(stack0)) {
-		puts("\ne: Underflow");
-		exit(2);
-	} //signal error
+    node_ptr tosp;
+    if (isEmpty(stack0)) {
+        puts("\ne: Underflow");
+        exit(2);
+    } //signal error
 
-	else {
-		STACK_DATA item;
-		tosp = stack0->next;
-		item = stack0->next->data;
-		stack0->next = stack0->next->next;
-		free(tosp);
-		return (item);
-	}
+    else {
+        STACK_DATA item;
+        tosp = stack0->next;
+        item = stack0->next->data;
+        stack0->next = stack0->next->next;
+        free(tosp);
+        return (item);
+    }
 }
 
 bool isEmpty(stack s) { return s->next == NULL; }
 
 bool is_operator(char symbol) {
-	if (symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-')
-		return 1;
-	else return 0;
+    if (symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-')
+        return 1;
+    else return 0;
 }
 
 bool is_plus(char symbol) {
-	if (symbol == '+' || symbol == '-')
-		return 1;
-	else return 0;
+    if (symbol == '+' || symbol == '-')
+        return 1;
+    else return 0;
 }
 
 short weight(char symbol) {
-	if (symbol == '*' || symbol == '/') return 2;
-	else if (symbol == '+' || symbol == '-') return 1;
-	else return 0;
+    if (symbol == '*' || symbol == '/') return 2;
+    else if (symbol == '+' || symbol == '-') return 1;
+    else return 0;
 }
 
 bool validate(const char infix[]) {
-	typedef char STACK_DATA;
-	short i = 0;
-	char item;
-	stack stack_v = newStack();
+//	typedef char STACK_DATA;
+    short i = 0;
+    char item;
+    stack stack_v = newStack();
 
-	while ((item = infix[i++]) != '\0') {
-		if (item == '(') push(stack_v, item);
+    while ((item = infix[i++]) != '\0') {
+        if (item == '(') push(stack_v, item);
 
-		else if (isdigit(item) || is_operator(item));
+        else if (isdigit(item) || is_operator(item));
 
-		else if (item == ')') {
-			if (isEmpty(stack_v))
-				return 0; //when ) > (
-			else pop(stack_v);
+        else if (item == ')') {
+            if (isEmpty(stack_v))
+                return 0; //when ) > (
+            else pop(stack_v);
 
-		}
-	}
+        }
+    }
 
-	if (isEmpty(stack_v)) return 1;
-	else return 0; // when ( > )
+    if (isEmpty(stack_v)) return 1;
+    else return 0; // when ( > )
 }
 
 short InfixConv(char infix[], char postfix[]) {
-	short i = 0, j = 0;
-	char item = infix[i];
-	char x;
-	stack stack0 = newStack();
+    short i = 0, j = 0;
+    char item = infix[i];
+    char x;
+    stack stack0 = newStack();
+    bool leading = 1; //used to determine leading sign
 
-	if (!validate(infix)) {
-		puts("e: Unbalanced bracket");
-		return -1;
-	}
+    if (!validate(infix)) {
+        puts("e: Unbalanced bracket");
+        return -1;
+    }
 
-	push(stack0, '(');                               /* push '(' onto stack */
-	strcat(infix, ")");                  /* add ')' to infix expression */
+    push(stack0, '(');                   /* push '(' onto stack */
+    strcat(infix, ")");                  /* add ')' to infix expression */
 
-	while (item != '\0') {
-		if (DEBUG)printf("\nCurrent Item %c %d\n", item, (int) item);
-		if (item == '(') push(stack0, item);
+    while (item != '\0') {
+        if (DEBUG) printf("\nCurrent Item %c %d\n", item, (int) item);
 
-		else if (isdigit(item)){
-		//postfix[j++]=' ';
-		do{
-		postfix[j++] = item;item=infix[++i];} 
-		while(isdigit(item));
-		
-		postfix[j++]=' ';
-		i--;
-		
-		
-}
+        if (item == '(') {
+            push(stack0, item);
+            leading = 1;
+        } else if (leading && is_plus(item)) {
+            if (item == '-') {
+                postfix[j++] = '0';
+                postfix[j++] = ' ';
+                //item = infix[++i];
+                //postfix[j++]=item;
 
-		else if (is_operator(item)) {
-			x = pop(stack0);
-			while (is_operator(x) && weight(x) >= weight(item)) { //pop all higher precedence opr
-				postfix[j++] = x;
-				x = pop(stack0);
-			}
-			push(stack0, x); //the above loop terminate and popped the failed element
-			push(stack0, item);
-		} else if (item == ')') {
-			x = pop(stack0);
-			while (x != '(') { //break if ( or \0 is encountered
-				postfix[j++] = x;
-				x = pop(stack0);
-			}
+                // -1 + -1
+                while (isdigit(item)){            //get the string
+                    postfix[j++] = item;
+                    item = infix[++i];
+                }
+
+                postfix[j++] = ' ';
+                postfix[j++] = '-';
+                postfix[j++] = ' ';
+                //i--;
+            }
+            item = infix[++i];
+            leading = 0;
+            continue;
+
+        } else if (isdigit(item)) {
+            leading = 0;
+            //postfix[j++]=' ';
+            do {
+                postfix[j++] = item;
+                item = infix[++i];
+            } while (isdigit(item));
+
+            postfix[j++] = ' ';
+            i--;
 
 
-		} else { //nothing match
-			puts("e: invalid symbol");
-			return -1;
-		}
-		item = infix[++i]; //move to next char
-	} //end while
+        } else if (is_operator(item)) {
+            leading = 1;                          //the next operator encountered will be leading sign
+            x = pop(stack0);
+            while (is_operator(x) && weight(x) >= weight(item)) { //pop all higher precedence opr
+                postfix[j++] = x;
+                x = pop(stack0);
+            }
+            push(stack0, x); //the above loop terminate and popped the failed element
+            push(stack0, item);
+        } else if (item == ')') {
+            x = pop(stack0);
+            while (x != '(') { //break if ( or \0 is encountered
+                postfix[j++] = x;
+                x = pop(stack0);
+            }
 
-	if (!isEmpty(stack0)) { //still non-empty stack?
-		puts("Unbalanced infix expression");
-		return -1;
-	}
 
-	postfix[j] = '\0'; //end string
-	return 1;
+        } else { //nothing match
+            puts("e: invalid symbol");
+            return -1;
+        }
+        item = infix[++i]; //move to next char
+    }           //end of while item!='\0'
+
+    if (!isEmpty(stack0)) { //still non-empty stack?
+        puts("Unbalanced infix expression");
+        return -1;
+    }
+
+    postfix[j] = '\0'; //end string
+    return 1;
 }
 
 short evaluate(const char postfix[], float *result) {
-	short i = 0,j=0;
-	float val, x1, x2;
-	stack stack3 = newStack();
-	char* currentS;
-	int current;
-	int opr;
+    short i = 0, j = 0;
+    float val, x1, x2;
+    stack stack3 = newStack();
+    char *currentS;
+    int current;
+    int opr;
 
-	while ((current = postfix[i++]) != '\0') {
-		if (current==' ') continue;
-		
-		if (isdigit(current)) {
-		currentS=malloc(sizeof(char)*20);
-			while (isdigit(current)){
-			//current -= '0';
-			currentS[j++]=current;
-			current=postfix[i++];
-			
-			//if (DEBUG) printf("\ncurrentS=%s", currentS);
-			}
-			currentS[j]='\0'; //end a string
-			if (DEBUG) printf("\ncurrentS=%s", currentS);
-			
-			
-			sscanf(currentS, "%d\n", &current); //similar to stoi()
-			if (DEBUG) printf("\ncurrent=%d", current);
-			
-			push(stack3, current);
-			free(currentS);
-			j=0;
-			continue;
-		}
-		//now assume is symbol
-	
-		x2 = pop(stack3);
-		x1 = pop(stack3);
-		val = x1;
+    while ((current = postfix[i++]) != '\0') {
+        if (current == ' ') continue;
 
-		opr = current;
-		if (DEBUG) printf("\nval=%.2f x1=%.2f x2=%.2f opr=%c", val, x1, x2, opr);
+        if (isdigit(current)) {
+            currentS = malloc(sizeof(char) * 20);
+            while (isdigit(current)) {
+                //current -= '0';
+                currentS[j++] = current;
+                current = postfix[i++];
 
-		switch (opr) {
-			case '+':
-				val += x2;
-				break;
-			case '-':
-				val -= x2;
-				break;
-			case '*':
-				val *= x2;
-				break;
+                //if (DEBUG) printf("\ncurrentS=%s", currentS);
+            }
+            currentS[j] = '\0'; //end a string
+            if (DEBUG) printf("\ncurrentS=%s", currentS);
 
-			case '/':
-				if (x2 == 0) {
-					puts("\ne: divide zero");
-					return (-1);
-				}
-				val /= x2;
-				break;
-				
-			default:
-				puts("\ne: invalid Symbol");
-				return (-1);
-		}
-		push(stack3, val);
-	}
-	*result = pop(stack3);
-	return 1;
+
+            sscanf(currentS, "%d\n", &current); //similar to stoi()
+            if (DEBUG) printf("\ncurrent=%d", current);
+
+            push(stack3, current);
+            free(currentS);
+            j = 0;
+            continue;
+        }
+        //now assume is symbol
+
+        x2 = pop(stack3);
+        x1 = pop(stack3);
+        val = x1;
+
+        opr = current;
+        if (DEBUG) printf("\nval=%.2f x1=%.2f x2=%.2f opr=%c", val, x1, x2, opr);
+
+        switch (opr) {
+            case '+':
+                val += x2;
+                break;
+            case '-':
+                val -= x2;
+                break;
+            case '*':
+                val *= x2;
+                break;
+
+            case '/':
+                if (x2 == 0) {
+                    puts("\ne: divide zero");
+                    return (-1);
+                }
+                val /= x2;
+                break;
+
+            default:
+                puts("\ne: invalid Symbol");
+                return (-1);
+        }
+        push(stack3, val);
+    }
+    *result = pop(stack3);
+    return 1;
 
 }//end of function
 
 int main() {
-	char s0[100] = "(5+3)/5+7*(2+3)";
-	char s1[100]; // 53+5/1+ = 2.6 , 53+5/723+*+=36.6
-	short feedback;
-	float result;
+    //char s0[200] = "(5+3)/5+7*(2+3)";
+    char s0[200] = "-1+-2++1";
+    char s1[200]; // 53+5/1+ = 2.6 , 53+5/723+*+=36.6
+    short feedback;
+    float result;
 
-	if(DEBUG){
-	char s3[50] = "((3*5)+(2+4)))"; //invalid
-	validate(s3) ? puts("Valid") : puts("Invalid");}
-
-
-	while (s0[0] != 'x') {
-		puts(s0);
-		feedback = InfixConv(s0, s1);
-		if (feedback > 0) {
-			puts(s1);
-		} else {
-			puts("Infix expression error. Try again!");
-			goto NEXT;
-		}//catch error
-
-		feedback = evaluate(s1, &result);
-		if (feedback > 0) printf("\nresult=%3.2f\n", result);
-
-		else puts("Postfix evaluation error. Try again!");
+    if (DEBUG) {
+        char s3[50] = "-1+-3"; //invalid
+        validate(s3) ? puts("Valid") : puts("Invalid");
+    }
 
 
-		NEXT:
-		puts("press enter...");
-		while (getchar() != '\n');
+    while (s0[0] != 'x') {
+        puts(s0);
+        feedback = InfixConv(s0, s1);
+        if (feedback > 0) {
+            puts(s1);
+        } else {
+            puts("Infix expression error. Try again!");
+            goto NEXT;
+        }//catch error
 
-		printf("\nPress 'x' to exit");
-		printf("\nPostfix_EXP>");
-		s0[0] = '\0';
-		gets(s0);
-	}
+        feedback = evaluate(s1, &result);
+        if (feedback > 0) printf("\nresult=%3.2f\n", result);
 
-	return 0;
+        else puts("Postfix evaluation error. Try again!");
+
+
+        NEXT:
+        puts("press enter...");
+        while (getchar() != '\n');
+
+        printf("\nPress 'x' to exit");
+        printf("\nPostfix_EXP>");
+        s0[0] = '\0';
+        gets(s0);
+    }
+
+    return 0;
 }
